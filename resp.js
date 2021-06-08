230ced3116c5a41de581b8b54b9704faf13ed207
abhinav2344 <85544295+abhinav2344@users.noreply.github.com>
2021-06-08 18:57:21 +0530

    Add files via upload

--- resp.js
+++ resp.js
@@ -11,4 +11,25 @@ burger.addEventListener('click', ()=>{
     navbar.classList.toggle('h-nav-resp');
     
 })
- 
+
+
+    var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
